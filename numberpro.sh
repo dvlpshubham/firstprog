e602b05fabe174d8d5f4c7c7115ede4438f893d6
Shubham <shubhamaryan805@gmail.com>
2022-06-15 20:14:58 +0530

    number program

---
+++ numberpro.sh
@@ -0,0 +1,27 @@
+#! /bin/bash -x
+
+isValue=true
+#1)
+if [ #check condition ]
+then
+        #body
+fi
+
+#2)
+if [ #check condition ]
+then
+        #body of if
+else
+        #body of else
+fi
+
+#3)
+if [ #check condition ]
+then
+        #body of if
+elif [ #second check condition ]
+then
+        #body of else if
+else
+        #body of second else
+fi
